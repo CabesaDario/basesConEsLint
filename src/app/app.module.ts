1d84eadcb009fd1c88130c102b0ce4eaa044d833
CabesaDario <darioojeda_1995@hotmail.com>
2021-06-29 12:50:28 +0200

    Creado componente Heroe para afianzar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorDarioComponent } from './contador-dario.component';
+import { ContadorDarioComponent } from './contador/contador-dario.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorDarioComponent
+    ContadorDarioComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
