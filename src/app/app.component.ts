d656c35fb101b29a3ff729f972de47976098959e
CabesaDario <darioojeda_1995@hotmail.com>
2021-06-29 12:03:00 +0200

    Commit con la funcionalidad inicial del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //nombre que este componente va a tener
+  templateUrl: 'app.component.html'
+  /*template: `<span>
+    <h1>Darío</h1>
+
+
+
+  </span>`,*/
+  //asi se hace con un template en la misma linea
 })
 export class AppComponent {
-  title = 'basesConEsLint';
+  //lo que nosotros tenemos por pantalla en el navegador es una instancia
+  //del AppComponent(es nuestro DOM)
+  public titulo: string = 'Contador App de Darío Ojeda Jurado';
+  public subtitulo: string = 'Siguiendo el curso de Fernando Herrera';
+  public numero: number = 0;
+  public   base: number = 5;
+
+  acumular( valor: number ){
+    this.numero += valor;
+  }
+
+ 
 }
