986904455dc371c98291ea22644dfe1a8f7be419
CabesaDario <darioojeda_1995@hotmail.com>
2021-07-02 11:02:10 +0200

    antes de la refactorización para usar el servicio
    se va a quitar la parte de los personajes del main page component para pasarla al servicio de dbz

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -9,24 +10,22 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent{
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder:  15000,
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500,
-    }
-  ];
+
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
 
+  get personajes(): Personaje[] {
+    return this.dbzService.personajes;
+  }
+
   agregarNuevoPersonaje( personaje: Personaje) {
-    console.log(personaje);
     this.personajes.push(personaje);
+  }
+
+  constructor( private dbzService: DbzService) {
+
   }
 
   
